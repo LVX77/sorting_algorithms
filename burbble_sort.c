999434ec5b79d3049b733268604e2eb14a4b2da8
Vitor Gabriel <vitorgmb2000@hotmail.com>
2021-04-15 22:41:27 -0300

    burbble_sort

---
+++ burbble_sort.c
@@ -0,0 +1,25 @@
+#include "library.h"
+#include <stdio.h>
+
+void insertion_sort(int *list, int size)
+{
+    int counter, counteraux, swap;
+    
+    counter = 0;
+    counteraux = 0;
+    while (counter < size - 1)
+    {
+        while (counteraux < size - counter - 1)
+        {
+            if(list[counteraux] > list[counteraux + 1])
+            {
+                swap = list[counteraux];
+                list[counteraux] = list[counteraux + 1];
+                list[counteraux + 1] = swap;
+            }
+            counteraux++;
+        }
+        counteraux = 0;
+        counter++;
+    }
+}
